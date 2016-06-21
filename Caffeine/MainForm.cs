1a60ca8e354ff6b63e95343533fce986d86e324a
Jonathan Meyer <jon@stejsoftware.com>
2016-06-21 07:50:05 -0400

    Initial Commit

---
+++ Caffeine/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Caffeine
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Icon = Caffeine.Properties.Resources.BeanIcon;
+            niBean.Icon = Caffeine.Properties.Resources.BeanIcon;
+            niBean.ShowBalloonTip(100, "Caffeine","Don't Go to sleep!", ToolTipIcon.None);
+        }
+    }
+}
